e875ebb3081cb2ce6cb6f585a719b0a248853c30
Hjalte Nielsen <hjalte33@gmail.com>
2018-04-12 11:40:10 +0200

    added new folders and structure to the repo

--- src/rl_agent/rl_agent.py
+++ src/rl_agent/rl_agent.py
@@ -7,21 +7,22 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 
-
-
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
 
-    # create a model
+    # create a test model to see if it will compile
     model = Sequential()
     model.add(Dense(units=64, activation='relu', input_dim=100))
     model.add(Dense(units=10, activation='softmax'))    
+    
+    # complie the model
     model.compile(loss='categorical_crossentropy',
                   optimizer='sgd',
                   metrics=['accuracy'])
 
+    # run the ros node
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
         rospy.loginfo(hello_str)
