e2b5bed11e1fb2742e03d76c6f545a49c9a7ee18
Buganla <Frederik.arentz@hotmail.com>
2018-04-26 09:55:23 +0200

    deleted useless crap

--- rl_moveit/src/planning_script.py
+++ rl_moveit/src/planning_script.py
@@ -6,8 +6,6 @@ import rospy
 import moveit_commander #allow us to communicate with the MoveIt Rviz interface
 import moveit_msgs.msg
 import geometry_msgs.msg
-import tf
-from tf.transformations import quaternion_from_euler
 #Here, we are just initializing the moveit_commander module.
 moveit_commander.roscpp_initialize(sys.argv)
 #Here, we are just initializing a ROS node.
@@ -22,12 +20,6 @@ group = moveit_commander.MoveGroupCommander("arm") #should be tool
 display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory)
 #Here we are creating a Pose object, which is the type of message that we will send as a goal. Then, we just give values to the variables that will define the goal Pose
 pose_target = geometry_msgs.msg.Pose()
-#pose_target.orientation = Quaternion(*quaternion_from_euler(0.0, 0.0, 0.0))
-pose_target.orientation = geometry_msgs.msg.Quaternion(*quaternion_from_euler(0.0, 0.0, 0.0)) #numpy.deg2rad(90.0)
-#pose_target.orientation.w = 3.0
-#pose_target.orientation.x = 0.0
-#pose_target.orientation.y = 0.0
-#pose_target.orientation.z = 0.0
 pose_target.position.x = 0.5
 pose_target.position.y = 0
 pose_target.position.z = 0.01
