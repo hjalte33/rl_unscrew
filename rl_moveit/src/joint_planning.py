9bca9e9595741b31381ff68162f8e6b71f2f8898
hjalte on common pc <hjalte33@gmail.com>
2018-05-14 12:57:34 +0200

    Minor updates

--- rl_moveit/src/joint_planning.py
+++ rl_moveit/src/joint_planning.py
@@ -12,12 +12,17 @@ rospy.init_node('move_bit', anonymous=True)
 
 robot = moveit_commander.RobotCommander()
 scene = moveit_commander.PlanningSceneInterface()    
-group = moveit_commander.MoveGroupCommander("tool")
+group = moveit_commander.MoveGroupCommander("arm")
 display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory)
 #Next, we are getting the current values of the joints.
 group_variable_values = group.get_current_joint_values()
 #Now, we modify the value of 1 of the joints, and set this new joint value as a target.
-group_variable_values[0] = -90
+group_variable_values[0] = 0
+group_variable_values[1] = -1.5707
+group_variable_values[2] = 1.5707
+group_variable_values[3] = -1.5707
+group_variable_values[4] = -1.5707
+group_variable_values[5] = 0
 group.set_joint_value_target(group_variable_values)
 #Finally, we just compute the plan for the new joint space goal.
 plan2 = group.plan()
